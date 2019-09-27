d627e947db63ab3e690595f43a72ed3d704135ef
Simon Nilsson <simon.nilsson123@gmail.com>
2019-09-27 13:40:22 +0200

    Auto stash before merge of "Simon" and "origin/dev"

---
+++ src/main/java/se/experis/spring_person/Greeting.java
@@ -0,0 +1,24 @@
+package se.experis.spring_person;
+
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
--- src/main/java/se/experis/spring_person/model/Person.java
+++ src/main/java/se/experis/spring_person/model/Person.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:src/main/java/se/experis/spring_person/model/Person.java
 package se.experis.spring_person.model;
 
 import java.util.ArrayList;
@@ -99,3 +100,94 @@ public class Person {
         this.relationList = relation;
     }
 }
+=======
+package se.experis.spring_person;
+
+import java.util.ArrayList;
+
+public class Person {
+
+
+    private String name;
+    private String personID;
+    private String lastName;
+    private ArrayList<String>  phoneIDList;
+    private ArrayList<String> emailList;
+    private Address address;
+
+   Person(String name, String personID, String lastName, ArrayList<String> phoneIDList, ArrayList<String> emailList, Address address){
+        this.name = name;
+        this.personID = personID;
+        this.lastName = lastName;
+        this.phoneIDList = phoneIDList;
+        this.emailList = emailList;
+        this.address = address;
+    }
+    public Person(){
+       /*name = "";
+       personID = "";
+       lastName = "";
+       phoneIDList = new ArrayList<>();
+       emailList = new ArrayList<>();
+       address = null;
+*/
+    }
+
+    public void personToString(){
+        System.out.print("\nName: " + name + "\nPerson ID: " + personID + "\nSurname: " + lastName + "\nPhone number: " + phoneIDList.get(0).toString());
+        for (int i = 1; i < phoneIDList.size(); i++) {
+            System.out.print("\n\t\t\t  " + phoneIDList.get(i).toString());
+        }
+        address.printAddress();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPersonID(String personID) {
+        this.personID = personID;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneIDList(ArrayList<String> phoneIDList) {
+        this.phoneIDList = phoneIDList;
+    }
+
+    public void setEmailList(ArrayList<String> emailList) {
+        this.emailList = emailList;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getName(){
+
+        return this.name;
+    }
+    public String getPersonID()
+    {
+        return this.personID;
+    }
+    public String getLastName(){
+
+        return this.lastName;
+    }
+    public ArrayList<String> getPhoneIDList()
+    {
+        return this.phoneIDList;
+    }
+
+    public Address getAddress()
+    {
+        return this.address;
+    }
+    public ArrayList<String> getEmailList(){
+    	return this.emailList;
+    }
+}
+>>>>>>> Stashed changes:src/main/java/se/experis/spring_person/Person.java
